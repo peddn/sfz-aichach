d0a41e44f888d58fcf2a188d9491a0a64a9c6bc3
peddn <public@peddn.net>
2020-03-21 18:53:04 +0100

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,16 @@ require('../scss/main.scss');
 
 import '@fortawesome/fontawesome-free/js/all'
 
-console.log('hello world');
+// handler whch hides a notification on a click
+function hideNotification() {
+    let notification = this.parentNode;
+    notification.classList.add("is-hidden");
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    // get all close buttons and register the click Event
+    let closeButtons = document.getElementsByClassName("delete");
+    for(let closeButton of closeButtons) {  
+        closeButton.addEventListener('click', hideNotification);
+    }
+});
