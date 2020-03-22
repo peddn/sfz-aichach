0150839950a80c723f6db93aad9fbd24add652db
peddn <public@peddn.net>
2020-03-22 09:21:52 +0100

    wip and clean up

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,30 @@ require('../scss/main.scss');
 
 import '@fortawesome/fontawesome-free/js/all'
 
-// handler whch hides a notification on a click
-function hideNotification() {
-    let notification = this.parentNode;
-    notification.classList.add("is-hidden");
-}
-
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", () => {
     // get all close buttons and register the click Event
-    let closeButtons = document.getElementsByClassName("delete");
-    for(let closeButton of closeButtons) {  
-        closeButton.addEventListener('click', hideNotification);
+    let closeButtons = document.querySelectorAll(".delete");
+
+    let navburgers = document.querySelectorAll(".navbar-burger");
+
+    console.log(closeButtons);
+
+    if (closeButtons.length > 0) {
+        closeButtons.forEach(button => {
+            button.addEventListener('click', function() {
+                let notification = this.parentNode;
+                notification.classList.add("is-hidden");
+            });
+        });
     }
+
+    if (navburgers.length > 0) {
+        navburgers.forEach(burger => {
+            const dataTarget = burger.dataset.target;
+            const target = document.getElementById(dataTarget);
+            burger.classList.toggle('is-active');
+            target.classList.toggle('is-active');
+        });
+    }
+
 });
